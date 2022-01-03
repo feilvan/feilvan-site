cec8e185f87f8b0503f6fa955bb0d47d9ae31fa8
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-01-03 13:56:33 +0700

    blur navbar backdrop & partial hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,38 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUndo } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUndo,
+  faArrowDown,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
   return (
-    <div className="px-4 py-8 bg-brand/20">
-      <div></div>
-      <h1 className="font-readex font-bold text-4xl tracking-tighter">
-        feilvan <FontAwesomeIcon icon={faUndo} />
-      </h1>
+    <div className="h-[36rem] overflow-hidden md:overflow-visible relative">
+      <div className="text-gray-200">
+        <div className="absolute -top-24 -right-16 text-[18rem] text-clip">
+          <FontAwesomeIcon icon={faArrowUp} />
+          <FontAwesomeIcon icon={faArrowDown} />
+        </div>
+        <FontAwesomeIcon
+          icon={faUndo}
+          className="absolute top-64 -right-20 text-[18rem] text-clip"
+        />
+      </div>
+      <div className="absolute -bottom-3 flex flex-col items-center w-full">
+        <h1 className="uppercase font-readex font-[300] text-gray-200 text-4xl tracking-widest">
+          realityreimagined
+        </h1>
+      </div>
+      <div className="absolute bottom-20 left-6">
+        <h1 className="font-readex font-bold text-brand text-8xl tracking-tighter">
+          feilvan
+        </h1>
+        <div className="mt-4  tracking-widest font-inter text-sm text-gray-600 space-y-1">
+          <h1>digital imaging</h1>
+          <h1>3d environment</h1>
+          <h1>front-end</h1>
+        </div>
+      </div>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,24 +4,24 @@ import { useRouter } from "next/router";
 export default function Navbar() {
   const router = useRouter();
   return (
-    <div>
-      <div className="layout flex flex-col items-center py-6 font-readex uppercase text-xs tracking-widest mx-auto max-w-5xl">
+    <div className="sticky top-0 z-10 backdrop-blur-md">
+      <div className="layout flex flex-col items-center py-6 font-readex font-semibold text-sm mx-auto max-w-5xl ">
         <div className="flex justify-between space-x-12">
           <Link href="/">
             <a className={router.pathname == "/" ? "navbar-active" : ""}>
-              Home
+              <a className="py-6">Home</a>
             </a>
           </Link>
           <Link href="/portfolio">
             <a
               className={router.pathname == "/portfolio" ? "navbar-active" : ""}
             >
-              Portfolio
+              <a className="py-6">Portfolio</a>
             </a>
           </Link>
           <Link href="/about">
             <a className={router.pathname == "/about" ? "navbar-active" : ""}>
-              About
+              <a className="py-6">About</a>
             </a>
           </Link>
         </div>
