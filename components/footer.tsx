7f8937fbfffc97d149b7a1ec6c3430422e5a5197
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-03-17 21:11:06 +0700

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,22 @@ export default function Footer() {
   const { theme, setTheme } = useTheme();
   return (
     <div className="">
-      <div className="relative sm:absolute bottom-0 flex flex-col place-items-center py-4 border-t-[1px] visible sm:invisible space-y-2">
-        <div className="flex flex-row space-x-4">
+      <div className="relative sm:absolute bottom-0 flex flex-col py-4 border-t-[1px] visible sm:invisible space-y-4">
+        <div className="space-y-4">
+          <button
+            type="button"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            {theme === "light" ? (
+              <div className="flex flex-row hover:underline">
+                Dark Mode <FaMoon size={20} className="ml-2 my-auto" />
+              </div>
+            ) : (
+              <div className="flex flex-row hover:underline">
+                Light Mode <FaSun size={20} className="ml-2 my-auto" />
+              </div>
+            )}
+          </button>
           <a
             href="https://www.figma.com/file/TVucbHo5Z3FfRoaiy2Ncp3/experimental"
             target="_blank"
@@ -20,16 +34,9 @@ export default function Footer() {
             target="_blank"
             className="flex flex-row"
           >
-            Source code
+            Source Code
             {<MdOutlineNorthEast size={20} className="ml-2 my-auto" />}
           </a>
-          <button
-            type="button"
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-            className="px-2 rounded-sm border-[1px] border-transparent hover:border-black dark:hover:border-white transition"
-          >
-            {theme === "light" ? <FaMoon size={15} /> : <FaSun size={15} />}
-          </button>
         </div>
         <div>© {new Date().getFullYear()} Fuad Elhasan Irfani</div>
       </div>
