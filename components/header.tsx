e58c869bad9427ac97c0717c57dfff2e7d1268fb
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-03-24 23:08:27 +0700

    Update header

--- components/header.tsx
+++ components/header.tsx
@@ -17,6 +17,9 @@ export default function Header() {
           FEILVAN
         </div>
         <div className=" mt-1 tracking-wide">Digital Imaging Artist</div>
+        <div className=" mt-1 text-xs text-neutral-500 tracking-wide">
+          Banyuwangi, Indonesia
+        </div>
       </div>
     </div>
   );
