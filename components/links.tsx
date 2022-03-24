e608e6cb49a7aedb6e125ebe358c77a0fe1b73a5
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-03-25 04:46:29 +0700

    Tidying list

--- components/links.tsx
+++ components/links.tsx
@@ -1,49 +1,42 @@
 import { FaYoutube, FaShoppingCart } from "react-icons/fa";
+import Example from "./popover_example";
+
+const list = [
+  {
+    title: "Editing Process",
+    subtitle: "YouTube",
+    href: "https://www.youtube.com/feilvan",
+    icon: <FaYoutube size={28} />,
+  },
+  {
+    title: "T-shirt, phone cases, and more",
+    subtitle: "TeePublic",
+    href: "https://www.teepublic.com/user/feilvan",
+    icon: <FaShoppingCart size={28} />,
+  },
+];
 
 export default function Links() {
   return (
     <div className="grid place-items-center mt-8 space-y-3">
-      <a
-        href="https://www.youtube.com/feilvan"
-        target="_blank"
-        className=" hover:no-underline"
-      >
-        <div
-          className="flex flex-row justify-between w-[90vw] sm:w-[36rem] px-4 py-4 bg-neutral-900 hover:bg-transparent rounded-md border border-transparent
+      {list.map((item) => (
+        <a href={item.href} target="_blank" className=" hover:no-underline">
+          <div
+            className="flex flex-row justify-between w-[90vw] sm:w-[36rem] px-4 py-4 bg-neutral-900 hover:bg-transparent rounded-md border border-transparent
         hover:border-neutral-500 transition"
-        >
-          <div className="space-y-1">
-            <div className="font-herosBold">Editing Process</div>
-            <div className="uppercase text-neutral-400 text-xs tracking-widest">
-              Youtube
+          >
+            <div className="space-y-1">
+              <div className="font-herosBold">{item.title}</div>
+              <div className="uppercase text-neutral-400 text-xs tracking-widest">
+                {item.subtitle}
+              </div>
             </div>
+            <div className="my-auto">{item.icon}</div>
           </div>
-          <div className="my-auto">
-            <FaYoutube size={28} />
-          </div>
-        </div>
-      </a>
+        </a>
+      ))}
 
-      <a
-        href="https://www.teepublic.com/user/feilvan"
-        target="_blank"
-        className=" hover:no-underline"
-      >
-        <div
-          className="flex flex-row justify-between w-[90vw] sm:w-[36rem] px-4 py-4 bg-neutral-900 hover:bg-transparent rounded-md border border-transparent
-        hover:border-neutral-500 transition"
-        >
-          <div className="space-y-1">
-            <div className="font-herosBold">T-shirt, phone cases, and more</div>
-            <div className="uppercase text-neutral-400 text-xs tracking-widest">
-              TeePublic
-            </div>
-          </div>
-          <div className="my-auto">
-            <FaShoppingCart size={28} />
-          </div>
-        </div>
-      </a>
+      <Example />
     </div>
   );
 }
