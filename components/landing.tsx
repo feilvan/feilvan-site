d49048f7bb32a78e7b7cf05198cf5aa82231240f
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-03-25 23:27:39 +0700

    simply finished

--- components/landing.tsx
+++ components/landing.tsx
@@ -2,13 +2,13 @@ import { FaArrowUp, FaArrowDown, FaUndo } from "react-icons/fa";
 
 export default function Landing() {
   return (
-    <div className=" flex flex-row h-screen w-screen">
-      <div className=" text-center my-auto w-full font-herosBold text-[9vw] sm:text-[3.6rem]">
+    <div className=" flex flex-col h-screen w-screen">
+      <div className=" text-center my-auto font-herosBold text-[9vw] sm:text-[3.6rem]">
         work{" "}
         <span className=" font-herosBoldItalic text-outline-white">in</span>{" "}
         progress...
       </div>
-      <div className=" absolute bottom-4 space-x-1 flex flex-row text-neutral-500">
+      <div className=" mx-auto mb-4 space-x-1 flex flex-row text-neutral-500">
         <div>© {new Date().getFullYear()} - </div>
         <div className="my-auto flex flex-row">
           <FaArrowUp size={15} />
