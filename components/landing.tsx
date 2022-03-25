cab98563544e55cf9435f08798cc2935e2f66e28
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-03-25 22:53:36 +0700

    init landing

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,7 +1,12 @@
 export default function Landing() {
   return (
-    <div>
-      <div>This is landing</div>
+    <div className=" flex flex-row h-screen w-screen">
+      <div className=" text-center my-auto w-full font-herosBold text-[2rem]">
+        work{" "}
+        <span className=" font-herosBoldItalic text-outline-white">in</span>{" "}
+        progress...
+      </div>
+      <div className="absolute w-full bottom-0 text-center">asd</div>
     </div>
   );
 }
