c81d5339287933432482374772bb4d1f691f57eb
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-03-25 10:10:20 +0700

    Tidying the links code & add info in certain row

--- components/header.tsx
+++ components/header.tsx
@@ -5,13 +5,8 @@ export default function Header() {
   return (
     <div className="mt-12">
       <div className="grid place-items-center">
-        <div className=" w-[10rem]">
-          <Image
-            src={profile}
-            layout="responsive"
-            placeholder="blur"
-            className=" rounded-full"
-          />
+        <div className=" w-[10rem] rounded-full overflow-hidden shadow-2xl shadow-cyan-500/60">
+          <Image src={profile} layout="responsive" placeholder="blur" />
         </div>
         <div className=" mt-8 font-herosBoldItalic text-3xl tracking-widest">
           FEILVAN
--- components/links.tsx
+++ components/links.tsx
@@ -1,4 +1,4 @@
-import { FaYoutube, FaShoppingCart } from "react-icons/fa";
+import { FaYoutube, FaShoppingCart, FaInfoCircle } from "react-icons/fa";
 
 const list = [
   {
@@ -6,25 +6,31 @@ const list = [
     subtitle: "YouTube",
     href: "https://www.youtube.com/feilvan",
     icon: <FaYoutube size={28} />,
+    noinfo: " ",
   },
   {
     title: "T-shirt, phone cases, and more",
     subtitle: "TeePublic",
     href: "https://www.teepublic.com/user/feilvan",
     icon: <FaShoppingCart size={28} />,
+    info: "I haven't updated the catalog for a while",
   },
 ];
 
 export default function Links() {
+  let isInfo = "";
   return (
     <div className="grid place-items-center mt-8 space-y-3">
       {list.map((item) => (
-        <a href={item.href} target="_blank" className=" hover:no-underline">
-          <div
-            className="flex flex-row justify-between w-[90vw] sm:w-[36rem] px-4 py-4 bg-neutral-900 hover:bg-transparent rounded-md border border-transparent
-        hover:border-neutral-500 transition"
-          >
-            <div className="space-y-1">
+        <a
+          href={item.href}
+          target="_blank"
+          className="w-[90vw] sm:w-[36rem] px-4 py-3 bg-neutral-900 hover:bg-transparent rounded-md border border-neutral-700
+        hover:border-neutral-500 transition hover:no-underline "
+        >
+          {(isInfo = item.noinfo)}
+          <div className="flex flex-row justify-between">
+            <div className="w-[80%] space-y-1">
               <div className="font-herosBold">{item.title}</div>
               <div className="uppercase text-neutral-400 text-xs tracking-widest">
                 {item.subtitle}
@@ -32,6 +38,12 @@ export default function Links() {
             </div>
             <div className="my-auto">{item.icon}</div>
           </div>
+          {!isInfo && (
+            <div className="flex flex-row px-2 py-2 mt-2 bg-white/10 rounded-md tracking-wide text-xs">
+              <FaInfoCircle className="my-auto mr-2" />
+              {item.info}
+            </div>
+          )}
         </a>
       ))}
     </div>
