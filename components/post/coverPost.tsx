a346e581b1bdbf1d9446724f45949e3dbc3ddbf2
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2023-01-11 17:35:08 +0700

    init

--- components/post/coverPost.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function CoverPost({ title, src }) {
-  return (
-    <div className="relative h-[40rem] w-full">
-      <Image src={src} alt={title} layout="fill" objectFit="cover" />
-    </div>
-  );
-}
--- components/post/date.tsx
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from "date-fns";
-
-export function DateID({ dateString }: { dateString: string }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "yyLLdd")}</time>;
-}
