257a762e6b09ba4c8f90b4dc9aa7f18cb9fe83dc
Fuad Elhasan Irfani <hello@feilvan.art>
2024-11-07 03:08:38 +0700

    fix

--- app/components/dialog.tsx
+++ app/components/dialog.tsx
@@ -2,8 +2,8 @@
 "use client";
 
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { X } from "lucide-react";
 import * as React from "react";
+import { LuX } from "react-icons/lu";
 
 import { cn } from "@/utils";
 
@@ -48,7 +48,7 @@ const DialogContent = React.forwardRef<
       >
         {children}
         <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-          <X className="h-4 w-4" />
+          <LuX className="h-4 w-4" />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
       </DialogPrimitive.Content>
