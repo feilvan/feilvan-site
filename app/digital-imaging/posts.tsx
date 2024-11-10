838605235d68ce149236a5f81d322be2b7c29dc2
Fuad Elhasan Irfani <hello@feilvan.art>
2024-11-10 21:38:51 +0700

    update link

--- app/digital-imaging/posts.tsx
+++ app/digital-imaging/posts.tsx
@@ -10,42 +10,42 @@ import post241020 from "@/images/digital-imaging/241020.jpg";
 const posts = [
   {
     date: "200315",
-    link: "https://www.instagram.com/feilvan",
+    link: "https://www.instagram.com/feilvan/p/B9wXfi4HI_W/",
     image: post200315,
   },
   {
     date: "200329",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/B-UansvnzJo/",
     image: post200329,
   },
   {
     date: "200424",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/B_Z8GjxHe-F/",
     image: post200424,
   },
   {
     date: "200509",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/B_9_OJTn7-U/",
     image: post200509,
   },
   {
     date: "200713",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/CC3YdBinFkI/",
     image: post200713,
   },
   {
     date: "201115",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/CHpzETdHZFa/",
     image: post201115,
   },
   {
     date: "220710",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/Cf33Y5JLNXr/",
     image: post220710,
   },
   {
     date: "241020",
-    link: "/digital-imaging",
+    link: "https://www.instagram.com/feilvan/p/DBYxe7TTDXd/",
     image: post241020,
   },
 ];
