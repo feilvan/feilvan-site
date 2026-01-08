16fa6c6ea2848c66915438ad4384f1b1aa09a86c
Fuad Elhasan Irfani <hello@feilvan.art>
2026-01-08 22:25:19 +0700

    hide some projects

--- src/components/nodes/3d.tsx
+++ src/components/nodes/3d.tsx
@@ -7,10 +7,10 @@ const items = [
   { title: "210914", image: "/images/3d/210914.webp" },
   { title: "210919", image: "/images/3d/210919.webp" },
   { title: "210922", image: "/images/3d/210922.webp" },
-  { title: "210930", image: "/images/3d/210930.webp" },
-  { title: "211002", image: "/images/3d/211002.webp" },
-  { title: "211105", image: "/images/3d/211105.webp" },
-  { title: "211128", image: "/images/3d/211128.webp" },
+  // { title: "210930", image: "/images/3d/210930.webp" },
+  // { title: "211002", image: "/images/3d/211002.webp" },
+  // { title: "211105", image: "/images/3d/211105.webp" },
+  // { title: "211128", image: "/images/3d/211128.webp" },
 ];
 
 export default function ThreeD() {
